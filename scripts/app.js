c50775acc90449701c25a4df646696f833610065
Julius Suominen <julius.suominen@siilim160707.local>
2016-08-10 17:32:24 +0300

    Mock data usage

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,43 @@
 angular.module('todoListApp', [])
+
 .controller('mainCtrl', function($scope, dataService) {
-
-	$scope.helloConsole = dataService.helloConsole;
-
 	$scope.learningNgChange = function() {
 		console.log("An input changed.");
 	};
 
-	$scope.todos = [
-		{"name": "task 1"},
-		{"name": "task 2"},
-		{"name": "task 3"},
-		{"name": "task 4"},
-		{"name": "task 5"}
-	]
+	$scope.helloWorld = dataService.helloWorld;
+
+	dataService.getTodos(function(response) {
+		console.log(response.data);
+		$scope.todos = response.data;
+	});
+
+	$scope.deleteTodo = function(todo, $index) {
+		dataService.deleteTodo(todo);
+		$scope.todos.splice($index, 1);
+	};
+
+	$scope.saveTodo = function(todo) {
+		dataService.saveTodo(todo);
+	};
+
 })
-.service('dataService', function(){
-	this.helloConsole = function() {
-		console.log('This is the hello console service!')
-	}
+.service('dataService', function($http){
+	this.helloWorld = function() {
+		console.log('This is the data service method!');
+	};
+
+	this.getTodos = function(callback) { 
+		$http.get('mock/todos.json')
+		.then(callback) 
+		};
+
+	this.deleteTodo = function(todo) {
+		console.log("The " + todo.name + " todo has been deleted!");
+	};
+
+	this.saveTodo = function(todo) {
+		console.log("The " + todo.name + " todo has been saved!");
+	};
+
 });
